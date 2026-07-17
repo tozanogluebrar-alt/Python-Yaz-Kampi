725ad36e8791d8ea623b4d3d86c40350fff213cc
tozanogluebrar-alt <tozanogluebrar@gmail.com>
2026-07-17 21:41:57 +0300

    yaş hesaplama kodu eklendi

--- ilk_kod.py
+++ ilk_kod.py
@@ -1,1 +1,12 @@
-print("Mühendislik temelleri atiliyor !")
+#kullanıcıdan doğum yılını metin olarak alıyoruz.
+dogum_yili_yazi = input("lütfen dogum yilinizi giriniz: ")
+
+#alınan metni matematiksel tam sayıya (int) dönüştürüyoruz.
+dogum_yili_sayi = int(dogum_yili_yazi)
+
+#yaş hesabı yapıyoruz (şu anki yıldan çıkarıyoruz)
+yas = 2026 - dogum_yili_sayi
+
+#sonucu ekrana yazdırıyoruz
+print("harika! hesaplamalarima göre suanki yasiniz : ")
+print(yas)
