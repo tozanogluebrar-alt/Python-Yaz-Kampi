a1f375e61d53c3d8d2b40861dac3921ea6ce5c33
tozanogluebrar-alt <tozanogluebrar@gmail.com>
2026-07-20 13:27:09 +0300

    Döngüler modülü bitirildi.

--- Donguler.c
+++ Donguler.c
@@ -1,11 +1,15 @@
 #include  <stdio.h>
 
 int main(){
-    // int i = 1 -> Başlangıç
-    // i <= 5    -> Şart (şart doğru olduğu sürece çalış)
-    // i++       -> Artış miktarı (Her turda i'yi 1 arttır.)
-    for(int i = 1; i <=5; i++ ){
-        printf("For Dongusu - sayi: %d\n", i);
-    }
+    int hedefSayi;
+    printf("Lutfen bir sayi giriniz: ");
+    scanf("%d", &hedefSayi);
+
+    // 1'den başlayıp kullanıcının girdiği sayıya kadar giden dinamik for döngüsü
+    for(int i = 1; i <= hedefSayi; i++ )
+    {
+        printf("sayi: %d\n", i);
+    } 
     return 0;
+
 }
