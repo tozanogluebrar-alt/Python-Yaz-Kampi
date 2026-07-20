8ca9fb8cc8f3842e9ce4dfce62ab6404138a05f2
tozanogluebrar-alt <tozanogluebrar@gmail.com>
2026-07-20 13:18:19 +0300

    Dinamik döngü pratikleri eklendi ve dosya güncellendi

--- Donguler.c
+++ Donguler.c
@@ -1,13 +1,11 @@
 #include  <stdio.h>
 
 int main(){
-    int sayi = 1;
-
-    // sayi 5'e eşit veya küçük olduğu sürece çalış
-    while (sayi <= 5)
-    {
-        printf("sayi: %d\n", sayi);
-        sayi++;       //sayıyı bir arttırır.(bu olmazsa sonsuz döngüye girer!)
+    // int i = 1 -> Başlangıç
+    // i <= 5    -> Şart (şart doğru olduğu sürece çalış)
+    // i++       -> Artış miktarı (Her turda i'yi 1 arttır.)
+    for(int i = 1; i <=5; i++ ){
+        printf("For Dongusu - sayi: %d\n", i);
     }
     return 0;
 }
