104674ac1f8684518a61171fb8a4b6a167d13f62
tozanogluebrar-alt <tozanogluebrar@gmail.com>
2026-07-20 23:19:59 +0300

    python while ve for döngüleri eklendi

---
+++ while_döngüsü.py
@@ -0,0 +1,10 @@
+#başlangıç değerimizi belirliyoruz (sayaç)
+sayac = 1
+
+# 2. döngü şartını yazıyoruz: sayac 5'e eşit veya küçük olduğu sürece çalış.
+while sayac <= 5:
+    print(f"{sayac}. kez: Python öğreniyorum! ")
+
+#3. çok önemli: sayacı 1 artırıyoruz ki döngü bir gün bitsin!
+
+    sayac = sayac + 1    
