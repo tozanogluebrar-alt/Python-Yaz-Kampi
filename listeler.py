ff24f4fe925e46dcb7379ac46e60b6a4d028050b
tozanogluebrar-alt <tozanogluebrar@gmail.com>
2026-07-21 00:36:17 +0300

    Listelerde dinamik eleman ekleme ve filtreleme örneği eklendi

--- listeler.py
+++ listeler.py
@@ -1,9 +1,16 @@
-#köşeli parantez kullanarak listemizi tamamlıyoruz.
-programlama_dilleri = ["Python", "C", "javaScript", "C++", "Go"]
+#Tüm dillerin olduğu ana liste 
+tum_diller = ["Python" , "C" , "javaScript", "C++", "C#", "Go"]
 
-print("--- Ekipte Kullanilan Diller ---")
+# C tabanlı dilleri toplayacağımız boş bir liste oluşturuyoruz
+c_tabanli_diller = []
 
-#2. for döngüsü ile listenin içindeki her bir elemanı sırayla 'dil' değişkenine atıyoruz
 
-for dil in programlama_dilleri:
-    print(f"Süper güç: {dil}")
+#döngü ile tüm dilleri tek tek kontrol ediyoruz.
+for dil in tum_diller :
+    #Eğer dilin içinde "C" harfi geçiyorsa (büyük-küçük harfe duyarlıdır)
+    if "C" in dil:
+        #boş listemizin sonuna bu dili dinamik olarak ekliyoruz.
+        c_tabanli_diller.append(dil)
+
+#döngü bittikten sonra filtrelenmiş yeni listemizi ekrana basıyoruz.
+print("Filtrelenmiş C Tabanlı Diller: ", c_tabanli_diller)        
