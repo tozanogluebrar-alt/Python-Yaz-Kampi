f950fd63be81831ad69a3bded0825789fc9b9bdc
tozanogluebrar-alt <tozanogluebrar@gmail.com>
2026-07-20 23:00:49 +0300

    Not sistemi için elif yapısı eklendi.

--- not_ortalamasi.py
+++ not_ortalamasi.py
@@ -1,8 +1,14 @@
-#önce değiken tanımlıyoruz ve bir değer atıyoruz.
-not_ortalamasi = 75
+#kullanıcıdan notu al.
+not_degeri = int(input("Notunuzu giriniz: "))
 
-# 2. koşulumuzu kontrol ediyoruz(sonuna : koymayı unutma)
-if not_ortalamasi >= 50:
-    print("Dersi gectiniz ! ")
+#koşulları sırasıyla kontrol ediyoruz.
+if not_degeri >= 90.0:
+    print("Harika ! Notun: AA")
+elif not_degeri >= 80:
+    print("çok iyi! Notun: BA")
+elif not_degeri >= 70:
+    print("iyi ! Notun: BA")     
+elif not_degeri >= 50:
+    print("Geçer ! Notun: CC") 
 else:
-    print("Kaldiniz , Maalesef ")    
+    print("Maalesef, kaldiniz. :(Notun: FF)")          
