3b964a98f52226d04876cf2ef3d04bbd348c5333
tozanogluebrar-alt <tozanogluebrar@gmail.com>
2026-07-21 00:18:20 +0300

    Listeler ve for kullanımı eklendi.

---
+++ listeler.py
@@ -0,0 +1,9 @@
+#köşeli parantez kullanarak listemizi tamamlıyoruz.
+programlama_dilleri = ["Python", "C", "javaScript", "C++", "Go"]
+
+print("--- Ekipte Kullanilan Diller ---")
+
+#2. for döngüsü ile listenin içindeki her bir elemanı sırayla 'dil' değişkenine atıyoruz
+
+for dil in programlama_dilleri:
+    print(f"Süper güç: {dil}")
