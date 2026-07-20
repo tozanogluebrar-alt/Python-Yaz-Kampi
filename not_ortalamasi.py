8416fc7fbaff0308a4d053704b42c26977bb0dd3
tozanogluebrar-alt <tozanogluebrar@gmail.com>
2026-07-20 22:42:59 +0300

    python if-else mantığıyla ilk kodlar

---
+++ not_ortalamasi.py
@@ -0,0 +1,8 @@
+#önce değiken tanımlıyoruz ve bir değer atıyoruz.
+not_ortalamasi = 75
+
+# 2. koşulumuzu kontrol ediyoruz(sonuna : koymayı unutma)
+if not_ortalamasi >= 50:
+    print("Dersi gectiniz ! ")
+else:
+    print("Kaldiniz , Maalesef ")    
