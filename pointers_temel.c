4748cff2bf6937568957ac078efb3ee3a07ea060
tozanogluebrar-alt <tozanogluebrar@gmail.com>
2026-07-21 18:59:27 +0300

    Pointer yapısı eklendi.

---
+++ pointers_temel.c
@@ -0,0 +1,19 @@
+#include  <stdio.h>
+
+int main(){
+    int sayi = 42;
+    // pointer tanımlama: int türündeki bir değişkenin adresini tutacak (*)
+    int *ptr = &sayi;
+
+    printf("sayinin Degeri                : %d\n", sayi);
+    printf("Sayinin Bellek Adresi         : %p\n", &sayi);         // %p adres basmak içindir
+    printf("pointer'in Tuttugu Adres      : %p\n", ptr);
+    printf("pointer'in Gosterdigi Deger   : %d\n\n", *ptr);
+
+    // pointer üzerinden değeri değiştirelim!
+    *ptr = 100;                                                   // ptr'nin gösterdiği adresteki değeri 100 yap
+    printf("Yeni Sayi Degeri          : %d\n" , sayi);
+
+    return 0;
+
+}
